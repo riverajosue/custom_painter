6a7e10c138ca0428a3c5f84caa50261db1391f4b
riverajosue <david.rivera@tcsvzla.com>
2023-02-23 20:04:01 -0400

    Header diagonal

--- lib/src/widgets/header_widget.dart
+++ lib/src/widgets/header_widget.dart
@@ -20,14 +20,59 @@ class RoundedEdgesHeader extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 300,
-     
-      decoration: BoxDecoration(
-         color: Color(0xFF615AAB),
-         borderRadius: BorderRadius.only(
-          bottomLeft: Radius.circular(70),
-          bottomRight: Radius.circular(70)
-         )
+      decoration: const BoxDecoration(
+          color: Color(0xFF615AAB),
+          borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(70),
+              bottomRight: Radius.circular(70))),
+    );
+  }
+}
+
+class DiagonalHeader extends StatelessWidget {
+  const DiagonalHeader({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: double.infinity,
+      width: double.infinity,
+      color: Color(0xFF615AAB),
+      child: CustomPaint(
+        painter: _diagonalPainterHeader(),
       ),
     );
+  }
+}
+
+class _diagonalPainterHeader extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    // TODO: implement paint
+
+    /// Lapiz
+    final paint = Paint();
+
+    ///Propiedades
+
+    paint.color = Colors.blueGrey;
+    paint.style = PaintingStyle.fill;
+    paint.strokeWidth = 5;
+
+    ///dibujar con el path y el lapiz
+    final path = new Path();
+
+    path.moveTo(0, size.height * 0.5);
+    path.lineTo(size.width, size.height * 0.45);
+    path.lineTo(size.width, 0);
+    path.lineTo(0,0);
+
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    // TODO: implement shouldRepaint
+    return true;
   }
 }
